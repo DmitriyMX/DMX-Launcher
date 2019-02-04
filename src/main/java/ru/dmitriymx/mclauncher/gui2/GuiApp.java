7f7019e4ed4c3cd98800ec8e76018ab37ef642cc
DmitriyMX <dimon550@gmail.com>
2019-02-05 00:43:42 +0300

    добавляем иконку

--- src/main/java/ru/dmitriymx/mclauncher/gui2/GuiApp.java
+++ src/main/java/ru/dmitriymx/mclauncher/gui2/GuiApp.java
@@ -4,6 +4,7 @@ import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
+import javafx.scene.image.Image;
 import javafx.stage.Stage;
 
 public class GuiApp extends Application {
@@ -21,6 +22,7 @@ public class GuiApp extends Application {
 
         primaryStage.setTitle("DmitriyMX Minecraft Server [" + LAUNCHER_VERSION + "]");
         primaryStage.setScene(new Scene(root));
+        primaryStage.getIcons().add(new Image(getClass().getResourceAsStream("/icon.png")));
         primaryStage.show();
     }
 }
